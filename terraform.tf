5ad40438b5aa5c008293daf5d99bbd67f13b93c7
rite93 <87079100+rite93@users.noreply.github.com>
2021-07-12 19:08:59 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,121 +1,145 @@
-provider "azurerm" {
-  features {}
-  subscription_id = "d1ed4df9-841a-4e20-a75f-8ec1e73c7f59"
-  client_id = "c36fdb6a-6958-4784-a26b-2456bc86b9c2"
-  client_secret="U.2KuPb8_2wwn3CqZAedFPgDzkulrgtuyu"
-  tenant_id = "654d8ab0-dbda-487d-b27a-bad697ae17b4"
-}
-
 resource "azurerm_resource_group" "example" {
-  name     = "resources"
+  name     = "example-resources"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "network"
-  resource_group_name = azurerm_resource_group.example.name
+  name                = "acctvn"
+  address_space       = ["13.0.0.0/16"]
   location            = azurerm_resource_group.example.location
-  address_space       = ["11.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.example.name
 }
 
-resource "azurerm_subnet" "internal" {
-  name                 = "internal"
+resource "azurerm_subnet" "example" {
+  name                 = "acctsub"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["11.0.2.0/24"]
+  address_prefixes     = ["13.0.2.0/24"]
 }
 
- resource "azurerm_monitor_autoscale_setting" "example" {
-  name                = "myAutoscaleSetting"
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_public_ip" "example" {
+  name                = "test"
   location            = azurerm_resource_group.example.location
-  target_resource_id  = azurerm_virtual_machine_scale_set.example.id
+  resource_group_name = azurerm_resource_group.example.name
+  allocation_method   = "Static"
+  domain_name_label   = azurerm_resource_group.example.name
 
-  profile {
-    name = "defaultProfile"
-
-    capacity {
-      default = 1
-      minimum = 1
-      maximum = 10
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "GreaterThan"
-        threshold          = 75
-        metric_namespace   = "microsoft.compute/virtualmachinescalesets"
-        dimensions {
-          name     = "AppName"
-          operator = "Equals"
-          values   = ["App1"]
-        }
-      }
-
-      scale_action {
-        direction = "Increase"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
-
-    rule {
-      metric_trigger {
-        metric_name        = "Percentage CPU"
-        metric_resource_id = azurerm_virtual_machine_scale_set.example.id
-        time_grain         = "PT1M"
-        statistic          = "Average"
-        time_window        = "PT5M"
-        time_aggregation   = "Average"
-        operator           = "LessThan"
-        threshold          = 25
-      }
-      scale_action {
-        direction = "Decrease"
-        type      = "ChangeCount"
-        value     = "1"
-        cooldown  = "PT1M"
-      }
-    }
+  tags = {
+    environment = "staging"
   }
 }
 
-resource "azurerm_windows_virtual_machine_scale_set" "example" {
-  name                = "vmss"
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_lb" "example" {
+  name                = "test"
   location            = azurerm_resource_group.example.location
-  sku                 = "Standard_F2"
-  instances           = 1
-  admin_password      = "P@55w0rd1234!"
-  admin_username      = "adminuser"
+  resource_group_name = azurerm_resource_group.example.name
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter-Server-Core"
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.example.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "bpepool" {
+  resource_group_name = azurerm_resource_group.example.name
+  loadbalancer_id     = azurerm_lb.example.id
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_pool" "lbnatpool" {
+  resource_group_name            = azurerm_resource_group.example.name
+  name                           = "ssh"
+  loadbalancer_id                = azurerm_lb.example.id
+  protocol                       = "Tcp"
+  frontend_port_start            = 50000
+  frontend_port_end              = 50119
+  backend_port                   = 22
+  frontend_ip_configuration_name = "PublicIPAddress"
+}
+
+resource "azurerm_lb_probe" "example" {
+  resource_group_name = azurerm_resource_group.example.name
+  loadbalancer_id     = azurerm_lb.example.id
+  name                = "http-probe"
+  protocol            = "Http"
+  request_path        = "/health"
+  port                = 8080
+}
+
+resource "azurerm_virtual_machine_scale_set" "example" {
+  name                = "mytestscaleset-1"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  # automatic rolling upgrade
+  automatic_os_upgrade = true
+  upgrade_policy_mode  = "Rolling"
+
+  rolling_upgrade_policy {
+    max_batch_instance_percent              = 20
+    max_unhealthy_instance_percent          = 20
+    max_unhealthy_upgraded_instance_percent = 5
+    pause_time_between_batches              = "PT0S"
+  }
+
+  # required when using rolling upgrade policy
+  health_probe_id = azurerm_lb_probe.example.id
+
+  sku {
+    name     = "Standard_F2"
+    tier     = "Standard"
+    capacity = 2
+  }
+
+  storage_profile_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "16.04-LTS"
     version   = "latest"
   }
 
-  os_disk {
-    storage_account_type = "Standard_LRS"
-    caching              = "ReadWrite"
+  storage_profile_os_disk {
+    name              = ""
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
   }
 
-  network_interface {
-    name    = "example"
+  storage_profile_data_disk {
+    lun           = 0
+    caching       = "ReadWrite"
+    create_option = "Empty"
+    disk_size_gb  = 10
+  }
+
+  os_profile {
+    computer_name_prefix = "testvm"
+    admin_username       = "azureuser"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/azureuser/.ssh/authorized_keys"
+      key_data = file("~/.ssh/id_rsa.pub")
+    }
+  }
+
+  network_profile {
+    name    = "terraformnetworkprofile"
     primary = true
 
     ip_configuration {
-      name      = "internal"
-      primary   = true
-      subnet_id = azurerm_subnet.internal.id
+      name                                   = "TestIPConfiguration"
+      primary                                = true
+      subnet_id                              = azurerm_subnet.example.id
+      load_balancer_backend_address_pool_ids = [azurerm_lb_backend_address_pool.bpepool.id]
+      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.lbnatpool.id]
     }
+  }
+
+  tags = {
+    environment = "staging"
   }
 }
