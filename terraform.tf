27ffa0a492f376a9183e61dc56e324f1a7f601c9
rite93 <87079100+rite93@users.noreply.github.com>
2021-07-12 19:44:06 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -36,7 +36,7 @@ resource "azurerm_subnet" "example" {
 
 resource "azurerm_public_ip" "example" {
   name                = "test"
-  location            = "East Europe"
+  location            = "East US"
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
   domain_name_label   = azurerm_resource_group.example.name
