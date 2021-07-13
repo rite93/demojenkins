19e68f859dc839f8a15ce8d5cb113b268a2e8bcd
rite93 <87079100+rite93@users.noreply.github.com>
2021-07-13 10:14:31 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -27,7 +27,7 @@ resource "azurerm_virtual_network" "example" {
   address_space       = ["11.0.0.0/16"]
 }
 
-resource "azurerm_subnet" "internal" {
+resource "azurerm_subnet" "example" {
   name                 = "internal"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
