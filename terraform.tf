2ee8324f3c6621cbd15bccac32cfe57d0390cfc9
rite93 <87079100+rite93@users.noreply.github.com>
2021-10-15 17:05:51 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -62,7 +62,6 @@ resource "azurerm_windows_virtual_machine_scale_set" "example" {
       name      = "internal"
       primary   = true
       subnet_id = azurerm_subnet.example.id
-      load_balancer_inbound_nat_rules_ids    = [azurerm_lb_nat_pool.example.id]
     }
   }
 
