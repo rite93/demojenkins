976d81e171330065808ae73fc73c34a1f3c7b526
rite93 <87079100+rite93@users.noreply.github.com>
2021-10-15 16:56:39 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -27,7 +27,7 @@ resource "azurerm_subnet" "example" {
 
 
 resource "azurerm_public_ip" "example" {
-  name                = "test"
+  name                = "IP"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
   allocation_method   = "Static"
