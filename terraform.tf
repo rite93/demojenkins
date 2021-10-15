908959dca49be72a71dc5af5be9d38868c6c3924
rite93 <87079100+rite93@users.noreply.github.com>
2021-10-15 17:01:41 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -30,10 +30,7 @@ resource "azurerm_lb" "example" {
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
 
-  frontend_ip_configuration {
-    name                 = "PublicIPAddress"
-    public_ip_address_id = azurerm_public_ip.example.id
-  }
+  
 }
 
 resource "azurerm_lb_nat_pool" "example" {
